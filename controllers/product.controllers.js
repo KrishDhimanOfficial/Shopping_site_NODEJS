05aeb018d9c768f2993b5612ba42f8840c9c0fa2
KRISH DHIMAN <krrishdhiman841@gmail.com>
2024-09-07 02:38:58 +0530

    completed product creation page

--- controllers/product.controllers.js
+++ controllers/product.controllers.js
@@ -2,6 +2,8 @@ const parent_Category = require('../Models/product_model/parent.category.model')
 const sub_Category = require('../Models/product_model/sub.category.model')
 const productSize = require('../Models/product_model/product.size.model')
 const productColor = require('../Models/product_model/product.color.model')
+const productBrand = require('../Models/product_model/product.brand.model')
+const product = require('../Models/product_model/product.model')
 
 module.exports = {
     // Product Category Controllers
@@ -42,6 +44,9 @@ module.exports = {
     },
     showAllCategories: async (req, res) => {
         try {
+            const color = await productColor.find({})
+            const size = await productSize.find({})
+            const brand = await productBrand.find({})
             const data = await parent_Category.aggregate([
                 {
                     $lookup: {
@@ -58,7 +63,12 @@ module.exports = {
                 },
                 { $unwind: '$sub_categories' }
             ])
-            res.render('admin/product/category', { categories: data })
+            res.render('admin/product/category', {
+                categories: data,
+                sizes: size,
+                brands: brand,
+                colors: color,
+            })
         } catch (error) {
             console.log(error.message);
         }
@@ -72,13 +82,40 @@ module.exports = {
     },
     // Product Category Controllers
     // Product Attributes Controllers
+    createProduct: async (req, res) => {
+        try {
+            const { product_title, product_parent_category_id, product_sub_category_id,
+                product_price, product_discount, product_stock, product_description,
+                availableColor, availableSize
+            } = req.body;
+            product_image = req.files.map(file => file.filename)
+
+            const data = await product.create({
+                product_title, product_parent_category_id, product_sub_category_id, product_image,
+                product_price, product_discount, product_stock, shipping: 'Free Shipping',
+                details: {
+                    product_description: product_description[1],
+                    availableColor, availableSize
+                }
+            })
+            if (!data) throw new Error('Unsuccessfull!')
+        } catch (error) {
+            console.log(error.message);
+        }
+    },
     showProductAttributes: async (req, res) => {
         try {
             const color = await productColor.find({})
             const size = await productSize.find({})
-            res.render('admin/product/create',{
+            const category = await parent_Category.find({})
+            const sub_category = await sub_Category.find({})
+            const brand = await productBrand.find({})
+            res.render('admin/product/create', {
                 sizes: size,
-                colors: color
+                colors: color,
+                brands: brand,
+                parent_Categories: category,
+                sub_categories: sub_category
             })
         } catch (error) {
             console.log(error.message);
@@ -102,7 +139,7 @@ module.exports = {
     createSize: async (req, res) => {
         try {
             const existingSize = await productSize.findOne({
-                size_name_name: { $regex: req.body.size_name, $options: "i" }
+                size_name: { $regex: req.body.size_name, $options: "i" }
             })
             if (existingSize) throw new Error('Already exists!')
 
@@ -112,5 +149,43 @@ module.exports = {
             console.log(error.message);
             res.json({ message: error.message ?? 'Unsuccessfull!' })
         }
-    }
+    },
+    deleteSize: async (req, res) => {
+        try {
+            await productSize.findByIdAndDelete({ _id: req.params.id })
+            res.json({ message: 'Successfully Deleted!' })
+        } catch (error) {
+            console.log(error.message);
+        }
+    },
+    deleteColor: async (req, res) => {
+        try {
+            await productColor.findByIdAndDelete({ _id: req.params.id })
+            res.json({ message: 'Successfully Deleted!' })
+        } catch (error) {
+            console.log(error.message);
+        }
+    },
+    createBrand: async (req, res) => {
+        try {
+            const existingBrand = await productBrand.findOne({
+                brand_name: { $regex: req.body.brand_name, $options: "i" }
+            })
+            if (existingBrand) throw new Error('Already exists!')
+
+            await productBrand.create(req.body)
+            res.json({ message: 'Successfully Created!' })
+        } catch (error) {
+            console.log(error.message);
+            res.json({ message: error.message ?? 'Unsuccessfull!' })
+        }
+    },
+    deleteBrand: async (req, res) => {
+        try {
+            await productBrand.findByIdAndDelete({ _id: req.params.id })
+            res.json({ message: 'Successfully Deleted!' })
+        } catch (error) {
+            console.log(error.message);
+        }
+    },
 }
